3c84772738ddacf77b5b2b8f50526891950daf3e
ljhsds-hh <130890319+ljhsds-hh@users.noreply.github.com>
2024-04-02 18:11:58 +0900

    Update ReturnInfo.xaml.cs

--- LibrarySystemForClient/LibrarySystemForClient/Ljh/Pages/ReturnInfo.xaml.cs
+++ LibrarySystemForClient/LibrarySystemForClient/Ljh/Pages/ReturnInfo.xaml.cs
@@ -1,4 +1,4 @@
-﻿using LibrarySystemForClient.Ljh.Model;
+using LibrarySystemForClient.Ljh.Model;
 using LibrarySystemForClient.Ljh.Utils;
 using LibrarySystemForClient.Ljh.Windows;
 using MaterialDesignThemes.Wpf;
@@ -30,33 +30,64 @@ namespace LibrarySystemForClient.Ljh.Pages
             InitDataGrid(1,12);
         }
 
-        
+
 
         private void ReturnBook_Click(object sender, RoutedEventArgs e)
         {
             var Button = (Button)sender as Button;
             var dataContent = Button.DataContext as Borrow;
-            MessageBoxResult IsReturn = MessageBox.Show("您确定要归还图书吗", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
-            if (IsReturn == MessageBoxResult.OK)
-            {
-                bool ReturnRes = ReturnBookUtil.IsSuccessReturnBook(dataContent.BookId);
-                if (ReturnRes)
+           
+                DateTime predictReturnBookTime = DateTime.Now;
+                DateTime.TryParse(dataContent.PredictReturnBookTime, out predictReturnBookTime);
+
+                if (predictReturnBookTime < DateTime.Now)
                 {
-                    MessageBoxResult result = MessageBox.Show("您已成功还书！", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
-                    if (result == MessageBoxResult.OK || result == MessageBoxResult.Cancel || result == MessageBoxResult.No)
+                    int OverDay = (DateTime.Now - predictReturnBookTime).Days;
+                    double punish = OverDay * 2.08;
+                    MessageBoxResult OverTimeReturn = MessageBox.Show("该图书您逾期未归还！！！需要缴纳 " + punish + "元 的罚款(每天需支付2.08元,具体为 " + OverDay + " * 2.08 = " + punish + ")，您确定您要归还吗", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                    if (OverTimeReturn == MessageBoxResult.OK)
                     {
-                        //获取已经显示的页面
-                        MainBook CurrentWindows = Application.Current.Windows.OfType<MainBook>().FirstOrDefault();
-                        if (CurrentWindows.mainFrame.Content is ReturnInfo returnInfo)
-                        {
-                            returnInfo.InitDataGrid(1, 12);
-                        }
+                        ReturnBookFun(dataContent.BookId);
+                        ReturnBookUtil.HandlePunishTable();
                     }
+                    else
+                    {
+                        MessageBoxResult OverTimeReturnToNext = MessageBox.Show("您拒绝了归还逾期的图书！您将无法在借书！！！", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                    }
+
                 }
                 else
                 {
-                    MessageBox.Show("还书失败", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                    MessageBoxResult IsReturn = MessageBox.Show("您确定要归还图书吗", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                    if (IsReturn == MessageBoxResult.OK)
+                    {
+                        ReturnBookFun(dataContent.BookId);
+                    }
+
                 }
+        }
+            
+    
+
+        private void ReturnBookFun(int bookId)
+        {
+            bool ReturnRes = ReturnBookUtil.IsSuccessReturnBook(bookId);
+            if (ReturnRes)
+            {
+                MessageBoxResult result = MessageBox.Show("您已成功还书！", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                if (result == MessageBoxResult.OK || result == MessageBoxResult.Cancel || result == MessageBoxResult.No)
+                {
+                    //获取已经显示的页面
+                    MainBook CurrentWindows = Application.Current.Windows.OfType<MainBook>().FirstOrDefault();
+                    if (CurrentWindows.mainFrame.Content is ReturnInfo returnInfo)
+                    {
+                        InitDataGrid(1, 12);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("还书失败", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
             }
         }
 
@@ -64,20 +95,23 @@ namespace LibrarySystemForClient.Ljh.Pages
         {
             var button = (Button)sender;
             var dataContext = button.DataContext as Borrow;
-            
-            
-            if (!BorrowBookUtil.IsOverdueReturnBook())
+
+            if (!OtherUtil.IsNeedHandlerNoExistBorrowInfo(dataContext.BookId))
             {
-                BookInfoWindow bookInfoWindow = new BookInfoWindow();
-                BorrowBookUtil.SetBookInfoValue(dataContext, bookInfoWindow);
-                bookInfoWindow.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                MessageBoxResult result = MessageBox.Show(
-                   "您有未归还图书，无法续借", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                if (!BorrowBookUtil.IsOverdueReturnBook())
+                {
+                    BookInfoWindow bookInfoWindow = new BookInfoWindow();
+                    BorrowBookUtil.SetBookInfoValue(dataContext, bookInfoWindow);
+                    bookInfoWindow.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBoxResult result = MessageBox.Show(
+                       "您有未归还图书，无法续借", "提醒", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                }
             }
         }
+
 
         public void InitDataGrid(int PageNum,int PageSize)
         {
